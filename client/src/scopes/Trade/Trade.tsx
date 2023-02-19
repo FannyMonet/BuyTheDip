b407f599899c557683071c4759b16a6c94a76a6c
Fanny Monet <monet.f@sfeir.com>
2023-02-19 19:11:40 +0100

    feat: statistics

--- client/src/scopes/Trade/Trade.tsx
+++ client/src/scopes/Trade/Trade.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useAppContext } from "../../contexts/AppContext";
 import "./Trade.css";
 type Order = {
@@ -12,10 +12,32 @@ export default function Trade() {
   const { token, username } = useAppContext();
   const [orders, setOrders] = useState<Order[]>([]);
   const [price, setPrice] = useState<number | null>(null);
+  const [expirationDate, setExpirationDate] = useState<string | null>(null);
+  const [expiredAtSum, setExpiredAtSum] = useState<number | null>(null);
   const [editPrice, setEditPrice] = useState<{
     price: number;
     id: string;
   } | null>(null);
+
+  const ordersAveragePrice = useMemo(
+    () => orders.reduce((sum, { price }) => sum + price, 0) / orders.length,
+    [orders]
+  );
+
+  const sumGroupByUser = useMemo(
+    () =>
+      Object.entries(
+        orders.reduce<Record<string, number>>((sumByUser, order) => {
+          return {
+            ...sumByUser,
+            [order.username]: sumByUser.hasOwnProperty(order.username)
+              ? sumByUser[order.username]
+              : 0 + order.price,
+          };
+        }, {})
+      ).sort(([, prevPrice], [, nextPrice]) => nextPrice - prevPrice),
+    [orders]
+  );
 
   useEffect(() => {
     fetch("http://localhost:8080/orders", {
@@ -34,6 +56,14 @@ export default function Trade() {
         setOrders(orders);
       });
   }, [reloadId]);
+
+  const sumExpiredAt = (date: Date) =>
+    orders
+      .filter(
+        ({ expirationDate }) =>
+          new Date(date).getTime() > new Date(expirationDate).getTime()
+      )
+      .reduce((sum, order) => sum + order.price, 0);
 
   const addOrder = (price: number) =>
     fetch("http://localhost:8080/orders", {
@@ -153,7 +183,34 @@ export default function Trade() {
           </tbody>
         </table>
       </div>
-      <div className="stats"></div>
+      <div className="stats">
+        <label>Expiration date</label>
+        <input
+          type="date"
+          value={expirationDate ?? ""}
+          onChange={({ target: { value } }) => setExpirationDate(value)}
+        ></input>
+        <button
+          onClick={() => {
+            if (expirationDate !== null) {
+              const sum = sumExpiredAt(new Date(expirationDate));
+              setExpiredAtSum(sum);
+            }
+          }}
+        >
+          Calculate sum
+        </button>
+        <p>
+          Sum of the item which expire at {expirationDate}: {expiredAtSum}
+        </p>
+        <p>Average price: {ordersAveragePrice}</p>
+        <p>Sums by users:</p>
+        <ul>
+          {sumGroupByUser.map(([username, sum]) => (
+            <li key={username}>{`${username}: ${sum}`}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
